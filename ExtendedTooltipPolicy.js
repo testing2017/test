dd6bec62ab5d153ba575875b7671c46202c49e36
Zhang Dan Jie <dannyec@users.noreply.github.com>
2018-02-02 10:35:41 +0800

    Create ExtendedTooltipPolicy.js

---
+++ ExtendedTooltipPolicy.js
@@ -0,0 +1,47 @@
+nx.define("ExtendedTooltipPolicy",nx.graphic.Topology.TooltipPolicy, {
+
+	"properties": {
+		topology: {},
+		tooltipManager: {}
+	},
+
+	"methods": {
+
+		// inherit methods and properties from base class (nx.graphic.Topology.TooltipPolicy)
+		"init": function(args){
+			this.inherited(args);
+			this.sets(args);
+			this._tm = this.tooltipManager();
+		},
+
+		// disable default behavior: "click" closes the tooltip
+		// "clickNode": function(node){
+		// 	this._tm.closeAll();
+		// },
+
+		// overwrite default behavior: hovering the node makes tooltip show up
+		"enterNode": function(node){
+			var topo = node.topology();
+			topo.tooltipManager().openNodeTooltip(node);
+		},
+
+		// overwrite default behavior: hovering the link makes tooltip show up
+		"enterLink": function(link){
+			var topo = link.topology();
+			topo.tooltipManager().openLinkTooltip(link);
+		},
+
+		// overwrite default behavior: leaving the node makes tooltip hide
+		"leaveLink": function(link){
+			this._tm.closeAll();
+		},
+
+		// overwrite default behavior: leaving the node makes tooltip hide
+		"leaveNode": function(node){
+			this._tm.closeAll();
+		}
+
+
+
+	}
+});
